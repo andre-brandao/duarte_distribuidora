075bd9043dc3d0ae5ff8a0c25992e9ac00d8ee96
andre-brandao <brandaoandre00@gmail.com>
2024-04-22 15:38:42 -0300

    supabase ssr

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,16 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { Database } from "$lib/supabase-types";
+import type { Session, SupabaseClient, User } from "@supabase/supabase-js";
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			supabase: SupabaseClient<Database>;
+			safeGetSession: () => Promise<{ session: Session | null; user: User | null }>;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
