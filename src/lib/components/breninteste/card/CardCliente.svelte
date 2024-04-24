82c0ffa9d521ce33a837718a8674dbe46e44e9ba
Breno Brandão <brenoca35@gmail.com>
2024-04-24 17:54:19 -0300

    modal cliente

---
+++ src/lib/components/breninteste/card/CardCliente.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+	import { Plus } from 'lucide-svelte';
+	import { Button } from '$lib/components/ui/button/index.js';
+
+	export let nome: string;
+	export let email: string;
+	export let celular: string;
+</script>
+
+<hr />
+<div class="flex py-3">
+	<!-- <div class="w-full flex-none md:w-auto">
+		<img
+			alt="Descrição da Imagem"
+			src={img}
+			class="h-16 w-16 rounded-lg object-cover md:h-20 md:w-20"
+		/>
+	</div> -->
+
+	<div class="flex-grow pl-4">
+		<div>
+			<h2 class="text-xl font-bold">{nome}</h2>
+			<h3 class="text-md text-gray-600">{celular}</h3>
+		</div>
+	</div>
+	<div class="w-full text-right">
+		<span class="block pb-3 text-xl font-bold">{email}</span>
+		<Button class="flex-none">
+			<p class="pr-2">Adicionar</p>
+			<Plus />
+		</Button>
+	</div>
+</div>
--- src/lib/components/modal/ModalCliente.svelte
+++ src/lib/components/modal/ModalCliente.svelte
@@ -3,13 +3,10 @@
 	import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
 	import * as Dialog from '$lib/components/ui/dialog/index.js';
 	import { Input } from '$lib/components/ui/input/index.js';
-	import CardProduto from '$lib/components/breninteste/card/CardProduto.svelte';
+	import CardCliente from '../breninteste/card/CardCliente.svelte';
 	import { searchTerm, categoriasUnicas, filteredProdutos } from '$lib/stores/filtroProdutosStore';
+	import { clientes } from '$lib/stores/clientesStore';
 	import { UserPlus } from 'lucide-svelte';
-
-	function setFilter(categoria: string) {
-		$searchTerm = categoria;
-	}
 </script>
 
 <Dialog.Root>
@@ -17,8 +14,26 @@
 		<ButtonCliente label={'Vincular compra a cliente'} Icon={UserPlus} />
 	</Dialog.Trigger>
 	<Dialog.Content class="flex h-[600px] overflow-hidden sm:max-w-[900px]">
-		<Dialog.Header class="sticky top-0 bg-white">
-			<Dialog.Title class="pr-3 text-center text-3xl">Vincular compra a um cliente</Dialog.Title>
-		</Dialog.Header>
+		<div class="w-full overflow-y-auto">
+			<Dialog.Header class="sticky top-0 bg-white">
+				<div class="gap-0 pb-5">
+					<div class=" flex items-center justify-between gap-0 pr-5">
+						<Dialog.Title class="pr-3 text-center text-3xl"
+							>Vincular compra a um cliente:</Dialog.Title
+						>
+						<Input
+							id="name"
+							placeholder="Pesquisar cliente..."
+							class="col-span-1 h-auto w-auto py-1"
+						/>
+					</div>
+				</div>
+			</Dialog.Header>
+			<div class="grid pr-5">
+				{#each $clientes as cliente (cliente.id)}
+					<CardCliente nome={cliente.nome} email={cliente.email} celular={cliente.celular} />
+				{/each}
+			</div>
+		</div>
 	</Dialog.Content>
 </Dialog.Root>
