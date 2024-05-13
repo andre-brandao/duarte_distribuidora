6ddebf545ee636de7d7101901573bd25b9f88727
Breno Brandão <brenoca35@gmail.com>
2024-05-13 18:17:35 -0300

    categoria barra + estilos

--- src/routes/cardapio/+page.svelte
+++ src/routes/cardapio/+page.svelte
@@ -59,69 +59,71 @@
 
 <div class="p-4 sm:ml-64">
 	<div class="">
-		<div class="mx-10 mb-2 mt-5 flex justify-between">
+		<div class="mb-5 mt-5 flex flex-col sm:flex-row justify-between">
 			<div class="items-center gap-0">
 				<h1 class="text-center text-4xl font-bold">Cardapio</h1>
 			</div>
-			<div class="flex justify-center gap-2">
+			<div class="flex justify-center gap-2 sm:mt-0 mt-4">
 				<Input
 					placeholder="Pesquisar produto..."
-					class="col-span-2 h-auto w-auto py-1"
+					class="col-span-2 md:h-auto h-10  py-1 w-full"
 					bind:value={search}
 				/>
 			</div>
 		</div>
-		<div
-			class=""
-		>
-			{#each categorias as categoria}
-			<div class="flex flex-col">
-				<div
-					id={categoria.id.toString()}
-					class="py-5 text-center text-[25px] font-bold"
-					use:intersect={{
-						root: document.documentElement,
-						rootMargin: '0px 0px 0px 0px',
-					}}
-					on:aa_enter={() => {
-						changeCategoriaSelecionada(categoria.id.toString());
-					}}
-				>
-					{categoria.nome}
-				</div>
-				<div class="grid grid-cols-[repeat(auto-fill,minmax(350px,1fr))] gap-2">
-					{#each categoria.var_produto as prod (prod.id)}
-						<ModalPedido
-							nome={prod.produto?.nome ?? 'Sem nome'}
-							preco={prod.preco?.[0]?.preco_in_cents}
-							categoria={categoria.nome}
-							img={prod.img_url ?? ''}
-						/>
+		
+
+		<div class="sticky top-2 z-10 bg-primary border rounded-lg">
+			<div
+				bind:this={scrollContainer}
+				class=" flex flex-row gap-1 overflow-auto p-2"
+			>
+				<div class="flex flex-row items-center gap-2">
+					{#each categorias ?? [] as categoria}
+						<a
+							href="#{categoria.id.toString()}"
+							id="cat-{categoria.id.toString()}"
+							on:click={scrollIntoView}
+							class="rounded-md px-3 py-2 transition duration-200 hover:bg-yellow-300"
+						>
+							<div>
+								{categoria.nome}
+							</div>
+						</a>
 					{/each}
 				</div>
 			</div>
-			{/each}
 		</div>
-	</div>
-</div>
 
-<div class="sticky top-0 z-10">
-	<div
-		bind:this={scrollContainer}
-		class="m-2 flex flex-row gap-1 overflow-auto p-2"
-	>
-		<div class="flex flex-row items-center gap-2">
-			{#each categorias ?? [] as categoria}
-				<!-- content here -->
-				<a
-					href="#{categoria.id.toString()}"
-					id="cat-{categoria.id.toString()}"
-					on:click={scrollIntoView}
-				>
-					<div>
+		<div class="">
+			{#each categorias as categoria}
+				<div class="flex flex-col">
+					<div
+						id={categoria.id.toString()}
+						class="py-5 text-center text-[25px] font-bold"
+						use:intersect={{
+							root: document.documentElement,
+							rootMargin: '0px 0px 0px 0px',
+						}}
+						on:aa_enter={() => {
+							changeCategoriaSelecionada(categoria.id.toString());
+						}}
+					>
 						{categoria.nome}
 					</div>
-				</a>
+					<div
+						class="grid grid-cols-[repeat(auto-fill,minmax(350px,1fr))] gap-2"
+					>
+						{#each categoria.var_produto as prod (prod.id)}
+							<ModalPedido
+								nome={prod.produto?.nome ?? 'Sem nome'}
+								preco={prod.preco?.[0]?.preco_in_cents}
+								categoria={categoria.nome}
+								img={prod.img_url ?? ''}
+							/>
+						{/each}
+					</div>
+				</div>
 			{/each}
 		</div>
 	</div>
