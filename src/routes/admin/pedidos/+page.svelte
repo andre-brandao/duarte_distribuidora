2ea79471bb82d0707855a0b5e96b5213a4cfc938
Breno Brandão <brenoca35@gmail.com>
2024-06-11 17:12:53 -0300

    pedidos filtro

--- src/routes/admin/pedidos/+page.svelte
+++ src/routes/admin/pedidos/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { formatDate, formatM } from '$lib/utils';
 	import type { PageData } from './$types';
+	import CardShowPedidos from './CardShowPedidos.svelte';
 
 	export let data: PageData;
 
@@ -8,12 +9,6 @@
 	$: ({ supabase } = data);
 
 	const pedidos = data.pedidos;
-
-	async function removeEstoque(id_pedido: number) {
-		const r = await supabase.rpc('process_pedido', { p_pedido_id: id_pedido });
-
-		console.log(r);
-	}
 
 	let pedidoSelecionado = 'all';
 
@@ -36,106 +31,53 @@
 	{JSON.stringify(pedidos, null, 2)}
 </pre> -->
 <div>
-	<h1 class="text-3xl font-bold">Pedidos:</h1>
-	<select
-		name="filtro"
-		id="filtro"
-		class="rounded-lg border bg-white p-2"
-		bind:value={pedidoSelecionado}
-	>
-		<option value="all">Todos pedidos</option>
-		<option value="aberto">Pendentes</option>
-		<option value="caminho">A caminho</option>
-		<option value="entregue">Entregue</option>
-	</select>
+	<div class="flex justify-between mb-4 items-center">
+		<h1 class="text-3xl font-bold">Pedidos:</h1>
+		<div class="mt-2">
+			<label for="filtro">Filtrar pedidos:</label>
+			<select
+				name="filtro"
+				id="filtro"
+				class="rounded-lg border bg-white p-2"
+				bind:value={pedidoSelecionado}
+			>
+				<option value="all">Todos pedidos</option>
+				<option value="aberto">Pendentes</option>
+				<option value="caminho">A caminho</option>
+				<option value="entregue">Entregue</option>
+			</select>
+		</div>
+	</div>
 	{#if pedidoSelecionado != 'all'}
 		{#each pedidosFiltrados as pedido}
-			<div class=" mt-2 rounded-lg border bg-gray-100 p-3 shadow-sm">
-				<div class="mb-4 grid grid-cols-3 gap-2 text-center text-sm">
-					<h3>Pedido <strong>#{pedido.id}</strong></h3>
-					<p>Tipo: <strong>{pedido.tipo}</strong></p>
-					<p>
-						Total: <strong class="text-green-500"
-							>R${formatM(pedido.total_in_cents)}</strong
-						>
-					</p>
-					<p>Data: {formatDate(pedido.created_at)}</p>
-					<p>Observacoes: {pedido.observacao ?? 'Nenhuma observacao'}</p>
-
-					<button
-						on:click={() => removeEstoque(pedido.id)}
-						class="rounded-md bg-red-500 px-4 py-2 text-white"
-					>
-						Processar pedido
-					</button>
-					<p>{pedido.status}</p>
-				</div>
-				<div>
-					<h4 class="mb-2 text-lg font-semibold">Produtos pedidos:</h4>
-					<div class="grid grid-cols-1 gap-2 sm:grid-cols-2 xl:grid-cols-3">
-						{#each pedido.produto_pedido as produto}
-							<div class="flex rounded-md bg-white p-2 shadow-sm">
-								<p class="text-gray-800">
-									{produto.quantidade}x -
-									{produto.var_produto?.produto?.nome}
-									{produto.var_produto?.categoria?.nome} -
-									<span class="font-bold">
-										R${formatM(produto.total_in_cents)}
-									</span>
-								</p>
-							</div>
-						{/each}
-					</div>
-				</div>
-			</div>
+			<CardShowPedidos {pedido} {supabase} />
 		{/each}
 	{:else}
 		<div class="grid grid-cols-1 gap-2 xl:grid-cols-3">
 			<div>
 				<h1 class="text-center">Pendentes:</h1>
 				{#each pedidos as pedido}
-					<div class=" mt-2 rounded-lg border bg-gray-100 p-3 shadow-sm">
-						<div class="mb-4 grid grid-cols-3 gap-2 text-center text-sm">
-							<h3>Pedido <strong>#{pedido.id}</strong></h3>
-							<p>Tipo: <strong>{pedido.tipo}</strong></p>
-							<p>
-								Total: <strong class="text-green-500"
-									>R${formatM(pedido.total_in_cents)}</strong
-								>
-							</p>
-							<p>Data: {formatDate(pedido.created_at)}</p>
-							<p>Observacoes: {pedido.observacao ?? 'Nenhuma observacao'}</p>
-
-							<button
-								on:click={() => removeEstoque(pedido.id)}
-								class="rounded-md bg-red-500 px-4 py-2 text-white"
-							>
-								Processar pedido
-							</button>
-							<p>{pedido.status}</p>
-						</div>
-						<div>
-							<h4 class="mb-2 text-lg font-semibold">Produtos pedidos:</h4>
-							<div class="grid grid-cols-1 gap-2 sm:grid-cols-2 xl:grid-cols-3">
-								{#each pedido.produto_pedido as produto}
-									<div class="flex rounded-md bg-white p-2 shadow-sm">
-										<p class="text-gray-800">
-											{produto.quantidade}x -
-											{produto.var_produto?.produto?.nome}
-											{produto.var_produto?.categoria?.nome} -
-											<span class="font-bold">
-												R${formatM(produto.total_in_cents)}
-											</span>
-										</p>
-									</div>
-								{/each}
-							</div>
-						</div>
-					</div>
+					{#if pedido.status === 'aberto'}
+						<CardShowPedidos {pedido} {supabase} />
+					{/if}
 				{/each}
 			</div>
-			<div><h1 class="text-center">A caminho:</h1></div>
-			<div><h1 class="text-center">Entregue:</h1></div>
+			<div>
+				<h1 class="text-center">A caminho:</h1>
+				{#each pedidos as pedido}
+					{#if pedido.status === 'caminho'}
+						<CardShowPedidos {pedido} {supabase} />
+					{/if}
+				{/each}
+			</div>
+			<div>
+				<h1 class="text-center">Entregue:</h1>
+				{#each pedidos as pedido}
+					{#if pedido.status === 'entregue'}
+						<CardShowPedidos {pedido} {supabase} />
+					{/if}
+				{/each}
+			</div>
 		</div>
 	{/if}
 </div>
---
+++ src/routes/admin/pedidos/CardShowPedidos.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+	import type { Json } from '$lib/supabase-types';
+	import { formatDate, formatM } from '$lib/utils';
+	import type { SupabaseClient } from '@supabase/supabase-js';
+
+	export let supabase: SupabaseClient;
+
+	export let pedido: {
+		cliente_id: number | null;
+		created_at: string;
+		distribuidora_id: number | null;
+		id: number;
+		meta_data: Json;
+		observacao: string | null;
+		status: string;
+		tipo: string;
+		total_in_cents: number;
+		produto_pedido: {
+			quantidade: number;
+			var_produto: {
+				id: number;
+				produto: {
+					created_at: string;
+					id: number;
+					nome: string;
+				} | null;
+				categoria: {
+					nome: string;
+				} | null;
+			} | null;
+			total_in_cents: number;
+		}[];
+	};
+
+	async function removeEstoque(id_pedido: number) {
+		const r = await supabase.rpc('process_pedido', { p_pedido_id: id_pedido });
+
+		console.log(r);
+	}
+</script>
+
+<div class=" mt-2 rounded-lg border bg-gray-100 p-2 shadow-sm">
+	<div class="mb-4 grid grid-cols-4 gap-1 text-center text-sm">
+		<h3>Pedido <strong>#{pedido.id}</strong></h3>
+		<p>Tipo: <strong>{pedido.tipo}</strong></p>
+		<p>
+			Total: <strong class="text-green-500"
+				>R${formatM(pedido.total_in_cents)}</strong
+			>
+		</p>
+		<p>Data: {formatDate(pedido.created_at)}</p>
+	</div>
+	<div>
+		<h4 class="mb-2 text-lg font-semibold">Produtos pedidos:</h4>
+		<div class="grid grid-cols-1 gap-2 sm:grid-cols-2 xl:grid-cols-3">
+			{#each pedido.produto_pedido as produto}
+				<div class="flex rounded-md bg-white p-2 shadow-sm">
+					<p class="text-gray-800">
+						{produto.quantidade}x -
+						{produto.var_produto?.produto?.nome}
+						{produto.var_produto?.categoria?.nome} -
+						<span class="font-bold">
+							R${formatM(produto.total_in_cents)}
+						</span>
+					</p>
+				</div>
+			{/each}
+		</div>
+	</div>
+    <div class="flex justify-between items-center text-sm mt-2">
+        <p>Observacoes: {pedido.observacao ?? 'Nenhuma observacao'}</p>
+        <button
+			on:click={() => removeEstoque(pedido.id)}
+			class="rounded-md bg-red-500 p-2 text-white"
+		>
+			Processar pedido
+		</button>
+    </div>
+</div>
