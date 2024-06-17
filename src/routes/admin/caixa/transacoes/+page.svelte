85664d5a15881b13a0fecf6d94e5248990a11970
Breno Brandão <brenoca35@gmail.com>
2024-06-17 15:44:35 -0300

    data table transacoes

--- src/routes/admin/caixa/transacoes/+page.svelte
+++ src/routes/admin/caixa/transacoes/+page.svelte
@@ -3,22 +3,36 @@
 	import type { PageData } from './$types'
 	import * as Table from '$lib/components/ui/table/index.js'
 	import { ArrowLeft } from 'lucide-svelte'
+	import DataTable from './data-table.svelte';
 
 	export let data: PageData
 
 	let transacao_caixa = data.transacao_caixa
+	//MOSTRAR UMA TABELA POR CAIXA E POR DISTRIBUIDORA
 </script>
 
 <main>
 	<div class="mb-7 flex items-center justify-between gap-5">
 		<h1 class="text-center text-4xl font-bold">Transações do caixa</h1>
-		<a
-			href="/admin/caixa"
-			class="flex gap-2 rounded-lg border p-2 px-5 font-bold transition ease-in-out hover:underline"
-			><ArrowLeft />Voltar</a
-		>
+		<div class="flex flex-col gap-5">
+			<a
+				href="/admin/caixa"
+				class="flex gap-2 rounded-lg border p-2 px-5 font-bold transition ease-in-out hover:underline"
+				><ArrowLeft />Voltar</a
+			>
+			<!-- <div class="gap-2">
+				<label for="filtro">Filtrar transacões:</label>
+				<select
+					name="filtro"
+					id="filtro"
+					class="rounded-lg border bg-white p-2"
+				>
+					<option value="all">Todas transacões</option>
+				</select>
+			</div> -->
+		</div>
 	</div>
-	<Table.Root>
+	<!-- <Table.Root>
 		<Table.Caption>Tabela transações do caixa!</Table.Caption>
 		<Table.Header>
 			<Table.Row>
@@ -34,7 +48,9 @@
 			{#each transacao_caixa as transacao}
 				<Table.Row>
 					<Table.Cell class="font-medium">{transacao.id}</Table.Cell>
-					<Table.Cell>{transacao.caixa?.distribuidora?.nome ?? 'Não tem'}</Table.Cell>
+					<Table.Cell
+						>{transacao.caixa?.distribuidora?.nome ?? 'Não tem'}</Table.Cell
+					>
 					<Table.Cell>{formatDate(transacao.created_at)}</Table.Cell>
 					<Table.Cell class="text-right"
 						>{transacao.meta_data?.tipo ?? 'Sem tipo'}</Table.Cell
@@ -48,7 +64,9 @@
 				</Table.Row>
 			{/each}
 		</Table.Body>
-	</Table.Root>
+	</Table.Root> -->
+
+	<DataTable data={transacao_caixa}/>
 </main>
 
 <pre>
---
+++ src/routes/admin/caixa/transacoes/data-table.svelte
@@ -0,0 +1,301 @@
+<script lang="ts">
+	import {
+		createTable,
+		Render,
+		Subscribe,
+		createRender,
+	} from 'svelte-headless-table'
+	import DataTableActions from './data-table-actions.svelte'
+	import { readable, writable, derived } from 'svelte/store'
+	import * as Table from '$lib/components/ui/table'
+	import type { Json } from '$lib/supabase-types'
+	import {
+		addPagination,
+		addSortBy,
+		addTableFilter,
+		addHiddenColumns,
+	} from 'svelte-headless-table/plugins'
+	import ArrowUpDown from 'lucide-svelte/icons/arrow-up-down'
+	import ChevronDown from 'lucide-svelte/icons/chevron-down'
+	import { Button } from '$lib/components/ui/button'
+	import { Input } from '$lib/components/ui/input'
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu'
+	import { formatM } from '$lib/utils'
+
+	export let data: Transacao[]
+
+	type Transacao = {
+		caixa_id: number
+		cents_transacao: number
+		created_at: string
+		id: number
+		meta_data: Json
+		total_log: number | null
+		caixa: {
+			cents_em_caixa: number
+			created_at: string
+			distribuidora_id: number
+			id: number
+			status: string
+			distribuidora: {
+				id: number
+				nome: string
+			} | null
+		} | null
+	}
+
+	let selectedCaixaId = writable<number | null>(null)
+
+	let uniqueCaixas = Array.from(
+		new Map(
+			data.map((transacao) => [transacao.caixa_id, transacao.caixa]),
+		).values(),
+	)
+
+	let filteredData = derived(
+		[readable(data), selectedCaixaId],
+		([$data, $selectedCaixaId]) => {
+			if ($selectedCaixaId === null) return $data
+			return $data.filter(
+				(transacao) => transacao.caixa_id === $selectedCaixaId,
+			)
+		},
+	)
+
+	const table = createTable(filteredData, {
+		page: addPagination(),
+		sort: addSortBy(),
+		filter: addTableFilter({
+			fn: ({ filterValue, value }) =>
+				value.toLowerCase().includes(filterValue.toLowerCase()),
+		}),
+		hide: addHiddenColumns(),
+	})
+
+	const columns = table.createColumns([
+		table.column({
+			accessor: 'id',
+			header: 'ID',
+			plugins: {
+				sort: {
+					disable: false,
+				},
+				filter: {
+					exclude: true,
+				},
+			},
+		}),
+		table.column({
+			accessor: 'meta_data',
+			header: 'Tipo',
+			cell: ({ value }) => {
+				const tipo = value?.tipo ? value?.tipo : 'Sem tipo'
+				return tipo
+			},
+			plugins: {
+				sort: {
+					disable: true,
+				},
+				filter: {
+					exclude: true,
+				},
+			},
+		}),
+		table.column({
+			accessor: 'caixa',
+			header: 'Distribuidora',
+			id: 'distribuidora_nome',
+			cell: ({ value }) => {
+				const caixa = value?.distribuidora?.nome
+					? value?.distribuidora.nome
+					: 'Sem distribuidora'
+				return caixa
+			},
+			plugins: {
+				sort: {
+					disable: true,
+				},
+			},
+		}),
+		table.column({
+			accessor: 'cents_transacao',
+			header: 'Valor da transacão',
+			cell: ({ value }) => {
+				const formatted = formatM(value)
+				return formatted
+			},
+			plugins: {
+				sort: {
+					disable: true,
+				},
+				filter: {
+					exclude: true,
+				},
+			},
+		}),
+		table.column({
+			accessor: 'total_log',
+			header: 'Total no caixa',
+			cell: ({ value }) => {
+				const formatted = formatM(value)
+				return formatted
+			},
+			plugins: {
+				sort: {
+					disable: false,
+				},
+				filter: {
+					exclude: true,
+				},
+			},
+		}),
+		// table.column({
+		// 	accessor: ({ id }) => id,
+		// 	header: '',
+		// 	cell: ({ value }) => {
+		// 		return createRender(DataTableActions, { id: value })
+		// 	},
+		// }),
+	])
+
+	const {
+		headerRows,
+		pageRows,
+		tableAttrs,
+		tableBodyAttrs,
+		pluginStates,
+		flatColumns,
+	} = table.createViewModel(columns)
+
+	const { hasNextPage, hasPreviousPage, pageIndex } = pluginStates.page
+	const { filterValue } = pluginStates.filter
+	const { hiddenColumnIds } = pluginStates.hide
+
+	const ids = flatColumns.map((col) => col.id)
+	let hideForId = Object.fromEntries(ids.map((id) => [id, true]))
+
+	$: $hiddenColumnIds = Object.entries(hideForId)
+		.filter(([, hide]) => !hide)
+		.map(([id]) => id)
+
+	const hidableCols = ['id', 'cents_transacao']
+</script>
+
+<div>
+	<div class="flex items-center gap-5 py-4">
+		<Input
+			class="max-w-sm"
+			placeholder="Filtrar por filial..."
+			type="text"
+			bind:value={$filterValue}
+		/>
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger asChild let:builder>
+				<Button variant="outline" class="ml-auto" builders={[builder]}>
+					Columns <ChevronDown class="ml-2 h-4 w-4" />
+				</Button>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content>
+				{#each flatColumns as col}
+					{#if hidableCols.includes(col.id)}
+						<DropdownMenu.CheckboxItem bind:checked={hideForId[col.id]}>
+							{col.header}
+						</DropdownMenu.CheckboxItem>
+					{/if}
+				{/each}
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
+		<select
+			on:change={(e) =>
+				selectedCaixaId.set(e.target.value ? parseInt(e.target.value) : null)}
+			class="flex h-9 rounded-md border border-input bg-white px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+		>
+			<option value="">Todos os caixas</option>
+			{#each uniqueCaixas as caixa,i}
+				<option value={caixa?.id}>
+					Caixa {caixa?.id} - {caixa?.distribuidora?.nome}
+				</option>
+			{/each}
+		</select>
+	</div>
+	<div class="rounded-md border">
+		<Table.Root {...$tableAttrs}>
+			<Table.Header>
+				{#each $headerRows as headerRow}
+					<Subscribe rowAttrs={headerRow.attrs()}>
+						<Table.Row>
+							{#each headerRow.cells as cell (cell.id)}
+								<Subscribe
+									attrs={cell.attrs()}
+									let:attrs
+									props={cell.props()}
+									let:props
+								>
+									<Table.Head {...attrs}>
+										{#if cell.id === 'total_log'}
+											<div class="text-right">
+												<Button variant="ghost" on:click={props.sort.toggle}>
+													<Render of={cell.render()} />
+													<ArrowUpDown class={'ml-2 h-4 w-4'} />
+												</Button>
+											</div>
+										{:else if cell.id === 'cents_transacao'}
+											<div class="text-right font-medium">
+												<Render of={cell.render()} />
+											</div>
+										{:else}
+											<Render of={cell.render()} />
+										{/if}
+									</Table.Head>
+								</Subscribe>
+							{/each}
+						</Table.Row>
+					</Subscribe>
+				{/each}
+			</Table.Header>
+			<Table.Body {...$tableBodyAttrs}>
+				{#each $pageRows as row (row.id)}
+					<Subscribe rowAttrs={row.attrs()} let:rowAttrs>
+						<Table.Row {...rowAttrs}>
+							{#each row.cells as cell (cell.id)}
+								<Subscribe attrs={cell.attrs()} let:attrs>
+									<Table.Cell {...attrs}>
+										{#if cell.id === 'total_log'}
+											<div class="text-right font-medium">
+												R$<Render of={cell.render()} />
+											</div>
+										{:else if cell.id === 'cents_transacao'}
+											<div class="text-right">
+												R$<Render of={cell.render()} />
+											</div>
+										{:else if cell.id === 'id'}
+											<div class="capitalize">
+												<Render of={cell.render()} />
+											</div>
+										{:else}
+											<Render of={cell.render()} />
+										{/if}
+									</Table.Cell>
+								</Subscribe>
+							{/each}
+						</Table.Row>
+					</Subscribe>
+				{/each}
+			</Table.Body>
+		</Table.Root>
+	</div>
+	<div class="flex items-center justify-end space-x-4 py-4">
+		<Button
+			variant="outline"
+			size="sm"
+			on:click={() => ($pageIndex = $pageIndex - 1)}
+			disabled={!$hasPreviousPage}>Página anterior</Button
+		>
+		<Button
+			variant="outline"
+			size="sm"
+			disabled={!$hasNextPage}
+			on:click={() => ($pageIndex = $pageIndex + 1)}>Próxima pagina</Button
+		>
+	</div>
+</div>
