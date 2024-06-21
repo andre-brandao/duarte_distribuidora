7a07158a95c1536576d59ae220f9de69ec2fcf48
Breno Brandão <brenoca35@gmail.com>
2024-06-21 19:41:48 -0300

    criacao de novo caixa

--- src/routes/admin/caixa/+page.svelte
+++ src/routes/admin/caixa/+page.svelte
@@ -1,13 +1,41 @@
 <script lang="ts">
+	import { toast } from 'svelte-sonner'
 	import type { PageData } from './$types'
 
 	export let data: PageData
 
+	let { supabase } = data
+	$: ({ supabase } = data)
+
 	let distribuidoras = data.distribuidoras
+
+	async function adicionarNovoCaixa(distribuidoraId: number) {
+		const { data, error } = await supabase
+			.from('caixa')
+			.insert([{ distribuidora_id: distribuidoraId }])
+			.select()
+
+		if (error) {
+			toast.error(error.message)
+			return
+		}
+
+		toast.success('Caixa adicionado com sucesso!')
+		distribuidoras = distribuidoras.map((distribuidora) => {
+			if (distribuidora.id === distribuidoraId) {
+				return {
+					...distribuidora, caixa: [...distribuidora.caixa, ...data],
+				}
+			}
+			return distribuidora
+		})
+	}
 </script>
 
 <main>
-	<div class="mb-7 flex items-center justify-between gap-5">
+	<div
+		class="mb-7 flex flex-col items-center justify-between gap-5 lg:flex-row"
+	>
 		<h1 class="text-center text-4xl font-bold">Selecione o caixa</h1>
 		<a
 			href="/admin/caixa/transacoes"
@@ -15,12 +43,17 @@
 			>Ver transacões dos caixas</a
 		>
 	</div>
-	<div class="grid grid-cols-2 justify-center gap-2">
+	<div class="grid grid-cols-1 justify-center gap-2 lg:grid-cols-2">
 		{#each distribuidoras as distribuidora}
-			<div>
+			<div class="flex flex-col gap-3">
 				<h1 class="min-w-48 rounded p-2 text-center text-xl">
 					{distribuidora.nome}:
 				</h1>
+				<button
+					class="text center mx-auto max-w-fit rounded-lg bg-primary p-2 px-4 transition ease-in-out hover:bg-opacity-80"
+					on:click={() => adicionarNovoCaixa(distribuidora.id)}
+					>Adicionar um novo caixa</button
+				>
 				<div class="flex flex-col gap-2 text-center">
 					{#each distribuidora.caixa as caixa}
 						<a
--- src/routes/cliente/+layout.svelte
+++ src/routes/cliente/+layout.svelte
@@ -9,7 +9,7 @@
 		<slot>Empty layout</slot>
 	</div>
 	<div
-		class="fixed bottom-0 left-0 right-0 flex h-24 justify-between bg-gray-200 bg-opacity-50"
+		class="fixed bottom-0 left-0 right-0 flex h-24 justify-between bg-gray-300 bg-opacity-50"
 	>
 		<button class="w-full text-center">cardapio</button>
 		<button class="w-full text-center">carrinho</button>
